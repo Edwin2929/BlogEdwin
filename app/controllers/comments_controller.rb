7a164fbc3f1f8014ca2f246dbdd2b62ab86048ee
Edwin <penaedwin299@gmail.com>
2018-11-26 15:44:45 -0600

    cambio

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,9 @@ class CommentsController < ApplicationController
 	 	@comment = @post.comments.create(params[:comment].permit(:name, :comment))
 		redirect_to post_path(@post)	
 	end
+	def update
+		@post = post.find
+	end
 
 	def destroy
 		@post = Post.find(params[:post_id])
